a606ab299f38724db80cbc41aa6be07a2ebda998
Fathan <74785057+Churma16@users.noreply.github.com>
2024-01-18 21:04:29 +0700

    feat(model, migration, factory): create model migration factory for post, postimages, category

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PostController>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
-class PostControllerFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
