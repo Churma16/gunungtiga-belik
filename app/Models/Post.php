5a90404886eb9d2d54712fa33c1d41002c34688d
Fathan <74785057+Churma16@users.noreply.github.com>
2024-01-18 21:10:24 +0700

    feat(Models): Add relation to all the models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded=['id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function postImage(){
+        return $this->hasOne(PostImage::class);
+    }
 }
